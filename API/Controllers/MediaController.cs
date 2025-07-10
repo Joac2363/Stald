84c77521c2508e20520c38a7f8dce9ea639c8b92
Joachim Storbank <joachimstorbank@gmail.com>
2025-07-10 08:37:54 +0200

    Added empty media controller

---
+++ API/Controllers/MediaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MediaController : ControllerBase
+    {
+        private readonly DbContext _context;
+        public MediaController(DbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
