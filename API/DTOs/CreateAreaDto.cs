dc817a6d4f850131f6369df91a63a73a18a08f5c
Joachim Storbank <joachimstorbank@gmail.com>
2025-07-21 13:31:02 +0200

    Added create dto

---
+++ API/DTOs/CreateAreaDto.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class CreateAreaDto
+    {
+        public string Name { get; set; }
+        [Required(ErrorMessage = "An Area must be connected to a Stable.")]
+        public int StableId { get; set; }
+    }
+}
