e0596de6f8bad2a1503c7011b8d09d717ca2cabb
Joachim Storbank <joachimstorbank@gmail.com>
2025-07-29 14:03:16 +0200

    Added UpdateAreaDto

---
+++ API/DTOs/UpdateAreaDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class UpdateAreaDto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "An Area must have a name.")]
+        [MaxLength(50, ErrorMessage = "An Area name cannot be longer than 50 characters.")]
+        public string Name { get; set; }
+    }
+}
