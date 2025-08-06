a73de8992eb986029d7e0555321ce367c851d90b
Joac2363 <joachimstorbank@gmail.com>
2025-08-06 17:33:40 +0200

    Changed swagger to froce start

--- API/Program.cs
+++ API/Program.cs
@@ -95,11 +95,11 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
