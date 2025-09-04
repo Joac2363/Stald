aeaaa1533811ac5867c6d0b2dbb467b406959955
Joac2363 <joachimstorbank@gmail.com>
2025-09-04 14:50:33 +0200

    Added stable to team model

--- API/Models/Team.cs
+++ API/Models/Team.cs
@@ -8,6 +8,11 @@ namespace API.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        public int StableId { get; set; }
+        [Required]
+        [ForeignKey(nameof(StableId))]
+        public Stable Stable { get; set; }
+        [Required]
         [StringLength(50)]
         public string Name { get; set; }
         [Required]
