fa583393ce6557a98a5a3529ae2578599a91782e
Joac2363 <joachimstorbank@gmail.com>
2025-09-04 14:46:35 +0200

    Added team model

---
+++ API/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public ICollection<User> TeamUsers { get;set; }
+    }
+}
