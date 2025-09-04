980db7cd6922fed5bbb782934424e0f01b463f33
Joac2363 <joachimstorbank@gmail.com>
2025-09-04 13:42:24 +0200

    Event migrations

---
+++ API/Migrations/20250904111631_AddedEvent.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SeriesId = table.Column<Guid>(type: "uuid", nullable: true),
+                    OriginalStartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    StableId = table.Column<int>(type: "integer", nullable: false),
+                    isOverride = table.Column<bool>(type: "boolean", nullable: false),
+                    isRecurring = table.Column<bool>(type: "boolean", nullable: false),
+                    RecurrenceRule = table.Column<string>(type: "text", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Events_Events_SeriesId",
+                        column: x => x.SeriesId,
+                        principalTable: "Events",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Events_Stables_StableId",
+                        column: x => x.StableId,
+                        principalTable: "Stables",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_EndDate",
+                table: "Events",
+                column: "EndDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_SeriesId",
+                table: "Events",
+                column: "SeriesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_StableId",
+                table: "Events",
+                column: "StableId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_StartDate",
+                table: "Events",
+                column: "StartDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
