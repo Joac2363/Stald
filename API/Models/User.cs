938b0a316baf2dc6798df9532d2cc3b94455fdc7
Joac2363 <joachimstorbank@gmail.com>
2025-09-07 14:39:06 +0200

    Added support for EventParticipation

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -7,6 +7,8 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Member Member { get; set; }
-        public ICollection<Team> TeamUsers { get; set; }
+        public ICollection<Team> TeamUsers { get; set; } = new List<Team>();
+        public ICollection<EventParticipation> EventParticipations { get; set; } = new List<EventParticipation>();
+
     }
 }
