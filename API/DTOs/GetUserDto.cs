9687fde0be754cb5dbab7d6926b7ed6376bb4e1e
Joac2363 <joachimstorbank@gmail.com>
2025-09-07 14:38:07 +0200

    Added GetUserDto

--- API/DTOs/GetUserDto.cs
+++ API/DTOs/GetUserDto.cs
@@ -4,6 +4,7 @@ namespace API.DTOs
 {
     public class GetUserDto
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
