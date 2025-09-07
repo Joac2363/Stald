e05a4a6dff07f15147cd561962870676b9fe1bad
Joac2363 <joachimstorbank@gmail.com>
2025-09-07 14:39:35 +0200

    Added EventParticipaiton Dtos

---
+++ API/DTOs/CreateEventParticipantsDto.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.DTOs
+{
+    public class CreateEventParticipantsDto
+    {
+        public ICollection<string> UserIds { get; set; } = new List<string>();
+        public ICollection<int> TeamIds { get; set; } = new List<int>();
+    }
+}
---
+++ API/DTOs/GetEventParticipationDto.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.DTOs
+{
+    public class GetEventParticipationDto
+    {
+        public Guid EventId { get; set; }
+        public ICollection<GetUserDto> Users { get; set; } = new List<GetUserDto>();
+        public ICollection<GetTeamDto> Teams { get; set; } = new List<GetTeamDto>();
+    }
+}
