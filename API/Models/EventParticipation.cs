aa5a62bba9407bd4ba37c55314749b2c883355d5
Joac2363 <joachimstorbank@gmail.com>
2025-09-07 14:38:29 +0200

    Added EventParticipation

---
+++ API/Models/EventParticipation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models
+{
+    public class EventParticipation
+    {
+        [Key]
+        [Required]
+        public Guid EventId { get; set; }
+        [ForeignKey(nameof(EventId))]
+        public Event Event { get; set; }
+        public ICollection<User> Users { get; set; } = new List<User>();
+        public ICollection<Team> Teams { get; set; } = new List<Team>();
+    }
+}
