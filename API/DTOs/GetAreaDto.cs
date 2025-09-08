e68df1016edb0fade59e4511580361738079e65c
Joac2363 <joachimstorbank@gmail.com>
2025-09-08 16:16:11 +0200

    Added GetAreaDto

---
+++ API/DTOs/GetAreaDto.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.DTOs
+{
+    public class GetAreaDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StableId { get; set; }
+        public bool IsGreenArea { get; set; }
+        public ICollection<Box> Boxes { get; set; }
+    }
+}
