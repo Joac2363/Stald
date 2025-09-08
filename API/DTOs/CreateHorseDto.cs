63e3d502330417f4dc36092f67956ad7a52591cc
Joac2363 <joachimstorbank@gmail.com>
2025-09-08 21:04:54 +0200

    Added horse dtos

---
+++ API/DTOs/CreateHorseDto.cs
@@ -0,0 +1,22 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.DTOs
+{
+    public class CreateHorseDto
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public bool IsOwnedByStable { get; set; }
+        public Guid? OwnerId { get; set; }
+        public int? BoxId { get; set; }
+        public int? GreenAreaId { get; set; }
+
+    }
+}
---
+++ API/DTOs/GetHorseDto.cs
@@ -0,0 +1,20 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.DTOs
+{
+    public class GetHorseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOwnedByStable { get; set; }
+        public GetUserDto? Owner { get; set; }
+        public int? BoxId { get; set; }
+        public string? BoxName { get; set; }
+        public int? GreenAreaId { get; set; }
+        public string? GreenAreaName { get; set; }
+
+    }
+}
